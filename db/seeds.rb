9d0d4774c23d7012c320ca65b84417e13c550751
Drew Keat <thedrewkeat@gmail.com>
2021-08-08 13:25:57 -0600

    Establish relationships between artist and songs, drop db remigrate and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+10.times do
+    artist = Artist.new(name: Faker::Music::RockBand.name)
+    10.times do
+        artist.songs.build(title: Faker::Music::RockBand.song)
+    end
+    artist.save
+end
